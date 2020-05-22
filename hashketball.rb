0ca17271f61259ac22e293a44d465248b4a8c794
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-05-22 20:12:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -7,47 +7,61 @@ def game_hash
       :colors => {["Black", "White"]},
       :players => {
         [
-          {:player_name
-            :number
-            :shoe
-            :points
-            :rebounds
-            :assists
-            :steals
-            :blocks
-            :slam_dunks
+          {
+            :player_name => "Alan Anderson",
+            :number => ,
+            :shoe => ,
+            :points => ,
+            :rebounds => ,
+            :assists => ,
+            :steals => ,
+            :blocks => ,
+            :slam_dunks >
           },
-          {:player_name
-            :number
-            :shoe
-            :points
-            :rebounds
-            :assists
-            :steals
-            :blocks
-            :slam_dunks
+          {
+            :player_name => "Reggie Evans",
+            :number => ,
+            :shoe => ,
+            :points => ,
+            :rebounds => ,
+            :assists => ,
+            :steals => ,
+            :blocks => ,
+            :slam_dunks =>
           },
-          {:player_name
-            :number
-            :shoe
-            :points
-            :rebounds
-            :assists
-            :steals
-            :blocks
-            :slam_dunks
+          {
+            :player_name => "Brook Lopez",
+            :number => ,
+            :shoe => ,
+            :points => ,
+            :rebounds => ,
+            :assists => ,
+            :steals => ,
+            :blocks => ,
+            :slam_dunks =>
           },
-          {:player_name
-            :number
-            :shoe
-            :points
-            :rebounds
-            :assists
-            :steals
-            :blocks
-            :slam_dunks
+          {
+            :player_name => "Mason Plumlee",
+            :number => ,
+            :shoe => ,
+            :points => ,
+            :rebounds => ,
+            :assists => ,
+            :steals => ,
+            :blocks => ,
+            :slam_dunks =>
           },
-
+          {
+            :player_name => "Jason Terry",
+            :number => ,
+            :shoe => ,
+            :points => ,
+            :rebounds => ,
+            :assists => ,
+            :steals => ,
+            :blocks => ,
+            :slam_dunks =>
+          }
         ]
       }
     },
