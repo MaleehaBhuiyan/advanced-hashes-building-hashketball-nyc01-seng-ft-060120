02d2b32d020c5b7cd128154a5cc7b17f3c0ba402
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-05-22 20:00:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,11 +3,15 @@ def game_hash
   {
     :home =>
     {
-
+      :team_name => {},
+      :colors => {},
+      :players => {}
     },
     :away =>
     {
-      
+      :team_name => {},
+      :colors => {},
+      :players => {}
     }
   }
 end
